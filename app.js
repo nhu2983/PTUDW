59907cd06d2ae38179809e40c1d75ffd6df8297a
nhu2983 <h.nhu2003@gmail.com>
2024-03-24 00:57:20 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const contactsRouter = require("./app/routes/contact.route")
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application. "});
+});
+appuse("/api/contacts", contactsRouter);
+
+module.exports = app;
+
